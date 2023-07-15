4659d9f4103a3c41237633ff10f811df9f3bb891
Madhavan1312000 <89676685+Madhavan1312000@users.noreply.github.com>
2023-07-15 21:32:33 +0530

    Update Contacts.jsx

--- src/component/Contacts.jsx
+++ src/component/Contacts.jsx
@@ -5,37 +5,20 @@ import axios from "axios"
 import { Link } from "react-router-dom"
 
 const Contacts=()=>{
-    let[datas,setDatas]=useState([])
-
-
-    useEffect(()=>{
-        axios.get("http://localhost:3000/datas")
-        .then((r)=>{
-            setDatas(r.data)
-        })
-        .catch(()=>{
-            console.log("Error")
-        })
-    },[])
 
     return(
         <div>
-            <div id={style.maindiv}>
-                {datas.map((x)=>{
-                    return(
-                        <div>
-                        <div id={style.d1}>
-                            <img src={x.image} alt="" />
-                            <h4>{x.name}</h4>
-                            <h3>{x.price}</h3>
-                           <Link to={`details/${x.id}`}>Add To Cart</Link>
-                        </div>
-                        </div>
-                    )
-                    
-                })}
-
-            </div>
+           <div id={style.maindiv3}>
+           <Link to="/home"><i class="fa-sharp fa-solid fa-circle-xmark" id={style.x}></i></Link>
+           <div id={style.m1}>
+            <ol>
+                <li><a href="https://www.whatsapp.com/" ><i class="fa-brands fa-square-whatsapp" id={style.i1}></i></a></li>
+                <li><a href="https://mail.google.com/" ><i class="fa-solid fa-envelope" id={style.i2}></i></a></li>
+                <li><a href="https://in.linkedin.com/" ><i class="fa-brands fa-linkedin" id={style.i3}></i></a></li>
+                <li><a href="https://www.instagram.com/" ><i class="fa-brands fa-square-instagram" id={style.i4}></i></a></li>
+            </ol>
+           </div>
+           </div>
         </div>
     )
 }
